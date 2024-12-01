2bc30097322a396138e94a3af80ae674344babef
badriawan <yusufbadriawan@gmail.com>
2024-12-01 08:27:38 +0700

    updated 1 Dec

---
+++ build/mapping/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/badri/new_ws/src/mapping
+# Build directory: /home/badri/new_ws/build/mapping
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/turtlebot3_simulations/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/badri/new_ws/src/turtlebot3_simulations/turtlebot3_simulations
+# Build directory: /home/badri/new_ws/build/turtlebot3_simulations
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
